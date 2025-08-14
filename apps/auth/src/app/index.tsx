42332577086ae8587c150205a6e7ba109168234c
aziz-akera <aziz@akera.agency>
2025-08-14 19:43:30 +0100

    setup tailwind and shadcn correctly

--- apps/auth/src/app/index.tsx
+++ apps/auth/src/app/index.tsx
@@ -1,11 +1,11 @@
 import { TanstackRouterProvider } from './providers/router-provider';
-import { ThemeProvider } from './providers/theme-provider';
+// import { ThemeProvider } from './providers/theme-provider';
 
 function App() {
   return (
-    <ThemeProvider>
-      <TanstackRouterProvider />
-    </ThemeProvider>
+    // <ThemeProvider>
+    <TanstackRouterProvider />
+    // </ThemeProvider>
   );
 }
 
--- apps/auth/src/pages/index.tsx
+++ apps/auth/src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@ui/components/button/button';
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button variant="default" size={'lg'} className="rounded-full">
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
